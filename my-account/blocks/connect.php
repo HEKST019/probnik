4d67e88c84eb1fdde37150a833165e5b8a9afe10
HEKST019 <alekzis019@gmail.com>
2025-10-02 21:08:38 +0400

    Delete my-account directory

--- my-account/blocks/connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-  $mysql = new mysqli('localhost', 'root', 'root', 'stolovka');
-  $mysql ->set_charset('utf8');
-
-?>
--- my-account/configDB.php
+++
@@ -1,4 +0,0 @@
-<?php
-$dsn = 'mysql:host=localhost;dbname=stolovka;charset=utf8';
-$pdo = new PDO($dsn, 'root', 'root');
-?>
